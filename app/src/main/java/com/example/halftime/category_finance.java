5594572f0cb5b60ec91958c1c9004630f9bafe6a
Daniel <djp297@cornell.edu>
2019-08-04 13:38:53 -0400

    Add all finance company classes to the finance class

--- app/src/main/java/com/example/halftime/category_finance.java
+++ app/src/main/java/com/example/halftime/category_finance.java
@@ -11,21 +11,32 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.halftime.companies.ABInbevActivity;
+import com.example.halftime.companies.AmericanCenturyActivity;
 import com.example.halftime.companies.AmericanExpressActivity;
+import com.example.halftime.companies.BAMLActivity;
+import com.example.halftime.companies.BarclaysActivity;
 import com.example.halftime.companies.ChurchDwightActivity;
+import com.example.halftime.companies.CitiActivity;
 import com.example.halftime.companies.CitibankActivity;
 import com.example.halftime.companies.CocaColaActivity;
 import com.example.halftime.companies.ColgateActivity;
+import com.example.halftime.companies.CreditActivity;
+import com.example.halftime.companies.DBActivity;
 import com.example.halftime.companies.DanoneActivity;
 import com.example.halftime.companies.EJGalloActivity;
+import com.example.halftime.companies.FidelityActivity;
 import com.example.halftime.companies.GeneralMillsActivity;
 import com.example.halftime.companies.HasbroActivity;
 import com.example.halftime.companies.HersheyActivity;
+import com.example.halftime.companies.IvyActivity;
 import com.example.halftime.companies.JPActivity;
 import com.example.halftime.companies.JohnsonJohnsonActivity;
 import com.example.halftime.companies.KimberlyClarkActivity;
 import com.example.halftime.companies.KraftHeinzActivity;
+import com.example.halftime.companies.LIMUActivity;
 import com.example.halftime.companies.LOrealActivity;
+import com.example.halftime.companies.MMCActivity;
+import com.example.halftime.companies.MTActivity;
 import com.example.halftime.companies.MarsActivity;
 import com.example.halftime.companies.MattelActivity;
 import com.example.halftime.companies.NestleActivity;
@@ -52,18 +63,18 @@ public class category_finance extends AppCompatActivity {
 
         //Create a list of company objects
         ArrayList<Company> companies = new ArrayList<Company>();
-        companies.add(new Company("American Century Investments", "Kansas City, MO", R.drawable.aci, new Intent(this, JPActivity.class)));
-        companies.add(new Company("Bank of America Merrill Lynch", "Charlotte, NC & New York, NY", R.drawable.baml, new Intent(this, JPActivity.class)));
-        companies.add(new Company("Barclays", "London, UK & New York, NY", R.drawable.barclays, new Intent(this, JPActivity.class)));
-        companies.add(new Company("Citi", "New York, NY", R.drawable.citi, new Intent(this, JPActivity.class)));
-        companies.add(new Company("Credit Suisse Group", "New York, NY", R.drawable.credit, new Intent(this, JPActivity.class)));
-        companies.add(new Company("Deutsche Bank", "New York, NY", R.drawable.db, new Intent(this, JPActivity.class)));
-        companies.add(new Company("Fidelity", "Boston, MA", R.drawable.fidelity, new Intent(this, JPActivity.class)));
-        companies.add(new Company("Ivy Investments", "Overland Park, KS", R.drawable.ivy, new Intent(this, JPActivity.class)));
+        companies.add(new Company("American Century Investments", "Kansas City, MO", R.drawable.aci, new Intent(this, AmericanCenturyActivity.class)));
+        companies.add(new Company("Bank of America Merrill Lynch", "Charlotte, NC & New York, NY", R.drawable.baml, new Intent(this, BAMLActivity.class)));
+        companies.add(new Company("Barclays", "London, UK & New York, NY", R.drawable.barclays, new Intent(this, BarclaysActivity.class)));
+        companies.add(new Company("Citi", "New York, NY", R.drawable.citi, new Intent(this, CitiActivity.class)));
+        companies.add(new Company("Credit Suisse Group", "New York, NY", R.drawable.credit, new Intent(this, CreditActivity.class)));
+        companies.add(new Company("Deutsche Bank", "New York, NY", R.drawable.db, new Intent(this, DBActivity.class)));
+        companies.add(new Company("Fidelity", "Boston, MA", R.drawable.fidelity, new Intent(this, FidelityActivity.class)));
+        companies.add(new Company("Ivy Investments", "Overland Park, KS", R.drawable.ivy, new Intent(this, IvyActivity.class)));
         companies.add(new Company("JP Morgan Chase & Co", "New York, NY", R.drawable.jpm, new Intent(this, JPActivity.class)));
-        companies.add(new Company("Liberty Mutual", "Boston, MA", R.drawable.limu, new Intent(this, JPActivity.class)));
-        companies.add(new Company("M&T Bank", "Buffalo, NY", R.drawable.mt, new Intent(this, JPActivity.class)));
-        companies.add(new Company("Marsh & McLennan", "New York, NY", R.drawable.mmc, new Intent(this, JPActivity.class)));
+        companies.add(new Company("Liberty Mutual", "Boston, MA", R.drawable.limu, new Intent(this, LIMUActivity.class)));
+        companies.add(new Company("M&T Bank", "Buffalo, NY", R.drawable.mt, new Intent(this, MTActivity.class)));
+        companies.add(new Company("Marsh & McLennan", "New York, NY", R.drawable.mmc, new Intent(this, MMCActivity.class)));
 
 
         //initialize a CompanyAdapter
